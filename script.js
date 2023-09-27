4aa52b572ac5d76d8c117e5a31f8f88927ef31ea
SaraEOlivera <saraeolivera@gmail.com>
2023-09-27 10:32:38 -0300

    Se agrega section work con function js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function onClick(image){
+    document.getElementById("imageZoom").src = image.src;
+    document.getElementById("modal").style.display = "block";
+    let imgText = document.getElementById("imgText");
+    imgText.innerHTML = image.alt;
+}
+
+
 
