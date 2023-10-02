ac603caeebce548fe3cac6b8c4987b5a198987db
SaraEOlivera <saraeolivera@gmail.com>
2023-10-02 10:12:21 -0300

    Se agrega responsive hasta 600px mas menu hamburguesa con JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Menu
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+});
+
 // Modals
 function onClick(image){
     document.getElementById("imageZoom").src = image.src;
